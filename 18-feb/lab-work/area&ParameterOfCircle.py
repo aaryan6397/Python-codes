8dd8bd5972875227beae3bc28570cb2a2b10597d
Aaryan <aryanparjapati9988@gmail.com>
2026-03-05 18:49:22 +0530

    update the output in each file

--- 18-feb/lab-work/area&ParameterOfCircle.py
+++ 18-feb/lab-work/area&ParameterOfCircle.py
@@ -7,6 +7,7 @@ radius = float(input("Enter the radius (in centimeter) :  "))
 # Check if the radius is greater than 0
 if(radius <= 0.0):
     print("radius should be greater then 0.0")
+    #
     exit()
 else:
     pie = 3.14
@@ -20,6 +21,6 @@ else:
 '''output:
 Enter the radius of the circle: 5
 area of circle 78.5
-area of parameter 31.400000000000002
+area of parameter 31.4000
 '''
      
