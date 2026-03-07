9f1084a34ee2f5cfe25c7e35db10f8d0109d31f5
Aaryan <aryanparjapati9988@gmail.com>
2026-03-07 14:22:42 +0530

    while loop to print numbers from 1 to N

---
+++ 24-feb/lab-work/21_print_1_To _N _Using_While_Loop.py
@@ -0,0 +1,19 @@
+# Problem 21
+# Problem Statement: Print numbers from 1 to N using while loop.
+# Input Format: Read input from standard input.
+# Output Format: Print the required output.
+# Constraints: Standard constraints apply.
+
+
+
+# enter the number...
+N = int(input("Enter a number : "))
+i=1
+while(N>=i):
+    print(i, end=",")
+    i+=1
+
+
+# output
+# Enter a number : 10
+# 1,2,3,4,5,6,7,8,9,10,
