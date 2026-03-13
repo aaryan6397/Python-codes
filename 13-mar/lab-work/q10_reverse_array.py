9efa4b84b7f07c544b083eec9f9d838c0fecd477
Aaryan <aryanparjapati9988@gmail.com>
2026-03-13 22:54:47 +0530

    Add NumPy program to reverse array from 1 to 10

--- 13-mar/lab-work/q10_reverse_array.py
+++ 13-mar/lab-work/q10_reverse_array.py
@@ -6,11 +6,17 @@
 import numpy as np
 
 numbers_array = np.arange(1, 11)
+print("Befor reversing array : ")
+print(numbers_array)
 
 reversed_array = numbers_array[::-1]
+print("After the reversing array : ")
 
 print(reversed_array)
 
 
 '''Output
-[10 9 8 7 6 5 4 3 2 1]'''
+Befor reversing array : 
+[ 1  2  3  4  5  6  7  8  9 10]
+After the reversing array :
+[10  9  8  7  6  5  4  3  2  1]'''
